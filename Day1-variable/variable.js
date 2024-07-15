d2c50594c9e518fba3c97564a3393457b5cc8d31
Trisha <trirox2030@gmail.com>
2024-07-15 23:21:35 +0530

    initial commit

---
+++ Day1-variable/variable.js
@@ -0,0 +1,45 @@
+//Activity 1 - variable declaration
+//Task 1
+var num = 56789;
+console.log(num);
+
+// Task 2
+let str1 = "Trisha";
+console.log(str1);
+
+// Activity 2 - Constatnt declaration
+// Task 3
+const flag = true;
+console.log(flag);
+
+//Activity 3 - Data types
+//Task 4
+let num2 = 2563;
+let str2 = "Raj";
+let employeed = false;
+let myobj = {
+  FirstName: "Trisha",
+  LastName: "Raj",
+  Profession: "Student",
+};
+let fruitArr = ["Apple", "Mango", "Grapes"];
+
+console.log(typeof num2);
+console.log(typeof str2);
+console.log(typeof employeed);
+console.log(typeof myobj);
+console.log(typeof fruitArr);
+
+//Activity 4 - Reassigning Variables
+//Task5
+let val;
+val = 123;
+console.log(val);
+val = 987;
+console.log(val);
+
+//Activity 5 - Understanding const
+//Task 6
+const roll = 101;
+console.log(roll);
+roll = 102; // TypeError: Assignment to constant variable
