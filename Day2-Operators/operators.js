51493e800eba825c6638d8129e5a31f64707d895
Trisha <trirox2030@gmail.com>
2024-07-16 14:25:04 +0530

    Day 2 updated

---
+++ Day2-Operators/operators.js
@@ -0,0 +1,58 @@
+// Activity 1 - Arithmetic operations
+//Task 1
+num1 = 10;
+num2 = 5;
+console.log(num1, " + ", num2, " = ", num1 + num2);
+
+// Task 2
+console.log(num1, " - ", num2, " = ", num1 - num2);
+
+//Task 3
+console.log(num1, " * ", num2, " = ", num1 * num2);
+
+// Task 4
+console.log(num1, " / ", num2, " = ", num1 / num2);
+
+//Task 5
+console.log(num1, " % ", num2, " = ", num1 % num2);
+
+// Activity 2 -   Assignment operators
+//Task 6
+let val = 5;
+val += 4;
+console.log(val);
+
+//Task 7
+let val2 = 10;
+val2 -= 2;
+console.log(val);
+
+//Activity 3 - Comparison operators
+//Task 8
+let a = 8,
+  b = 5;
+console.log("Greater than : ", a > b);
+console.log("Less than : ", a < b);
+
+//Task 9
+console.log("Greater than or equal to : ", a >= b);
+console.log("Less than or equal to : ", a <= b);
+
+//Task 10
+console.log("Equal to : ", a == b);
+console.log("Triple equal to : ", a === b);
+
+//Activity 4 - Logical operators
+// Task 11
+console.log(a > 1 && b > 2);
+
+// Task 12
+console.log(a == 5 || b == 2);
+
+// Task 13
+console.log(!(a == 2));
+
+// Activity 5 - Ternary operator
+//Task 14
+let res = a > 0 ? "positive" : "negative";
+console.log(res);
